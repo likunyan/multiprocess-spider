7967dbd90eb95e7942cf1e3d814eaf95de3a25d6
小李世界 <327125038@qq.com>
2015-05-14 22:24:19 +0800

    Update urllib2_pool.py

--- urllib2_pool.py
+++ urllib2_pool.py
@@ -13,7 +13,7 @@ import time
 # import socket
 # urllib2.socket.setdefaulttimeout(60) # Python2.6以前的版本
 
-def myPool(name):
+def myPool(source_file_name):
 	def myPoolmain(line):
 		line = line.replace("\n", "")  # 替换上一步中，轮询到的每行结果中的换行字符为空白
 		reqUrl = "http://"+line  # 因为self*的域名是不带http://的，这边加下
@@ -21,8 +21,9 @@ def myPool(name):
 			response = urlopen(reqUrl)
 			#time.sleep(N)  #等待N(N值暂未测量)秒，以免Web服务器频繁挂掉连接
 		except Exception, e:
-			with open(resultFile, 'a') as output:
+			with open(result_file_name, 'a') as output:
 				output.write("url_Error "+str(e)+" "+reqUrl+"\n")
+			return 0
 		except:  # beta版本代码
 			print "0"
 		else:
@@ -30,7 +31,7 @@ def myPool(name):
 				html = urllib2.urlopen(reqUrl, timeout=60).read()
 			except Exception, x:
 				# 保存错误到文件中去
-				with open(resultFile, 'a') as output:
+				with open(result_file_name, 'a') as output:
 					output.write("http_Error "+str(x)+" "+reqUrl+"\n")
 			except:  # beta版本代码
 				print "1"
@@ -42,10 +43,10 @@ def myPool(name):
 					#print "Yes,It's utf8"
 					htmlIsutf8 = html
 					if "百家乐" in htmlIsutf8:
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write("违规信息-百家乐"+" "+reqUrl+"\n")
 					elif "太阳城" in htmlIsutf8:
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write("违规信息-太阳城"+" "+reqUrl+"\n")
 					# 因为有的标题是多行的，保存起来有问题，所以这边去掉一切换行
 					htmlIsutf8 = string.replace(htmlIsutf8, '\r\n', '');
@@ -55,18 +56,18 @@ def myPool(name):
 					#if m:
 						#print m.group()
 					if m:
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write(m.group(1)+" "+reqUrl+"\n")
 						#print htmlIsutf8;
 					else:  # 特殊标题的标记
 						# <title xmlns=...><title> 个人用
 						m = re.search(r'<title xmlns="">(.*)</title>', htmlIsutf8, flags=re.I)
 						if m:
-							with open(resultFile, 'a') as output:
+							with open(result_file_name, 'a') as output:
 								output.write(m.group(1)+" "+reqUrl+"\n")
 							#print htmlIsutf8;
 						else:
-							with open(resultFile, 'a') as output:
+							with open(result_file_name, 'a') as output:
 								output.write("error"+" "+reqUrl+"\n")
 				else:
 					#print "No utf8 "
@@ -77,35 +78,37 @@ def myPool(name):
 					#if m:
 						#print m.group()
 					if "百家乐" in htmlNoutf8:
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write("违规信息-百家乐"+" "+reqUrl+"\n")
 					elif "太阳城" in htmlNoutf8:
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write("违规信息-太阳城"+" "+reqUrl+"\n")
 					if m:  # 如果标题不为空 则真，否则为假
-						with open(resultFile, 'a') as output:
+						with open(result_file_name, 'a') as output:
 							output.write(m.group(1)+" "+reqUrl+"\n")
 						# print htmlNoutf8;
 					else:
 						m = re.search(r'<title xmlns="">(.*)</title>', htmlNoutf8, flags = re.I)
 						if m:
-							with open(resultFile, 'a') as output:
+							with open(result_file_name, 'a') as output:
 								output.write(m.group(1)+" "+reqUrl+"\n")
 						else:
-							with open(resultFile, 'a') as output:
+							with open(result_file_name, 'a') as output:
 								output.write("error"+" "+reqUrl+"\n")
-	print "进程"+name+"开始"
+	print "进程"+source_file_name+"开始"
 	# 以下两行引用文件和输出文件!
-	file = name
-	resultFile = name+"-result"  # 多(N)进程的执行结果保存到各自的result文件中去
-	for line in open(file):  # 轮询self*文件中的网址
-		print line
-		myPoolmain(line)
-	print "进程"+name+"结束"
+	source_file = source_file_name # 这边定义下两行要打开的源文件
+	result_file_name = source_file_name+"-result"  # 多(N)进程的执行结果保存到各自的result结果文件中去
+	for line in open(source_file):  # 轮询源文件中的网址
+		hostValue = line.split() # 用空格分割字符串，并保存到列表
+		status = myPoolmain(hostValue[0])
+		if status == 0: # 如果source_file_name这个文本中第一列的网址能够访问的话，执行第二列中的网址
+			myPoolmain(hostValue[1])
+	print "进程"+source_file_name+"结束"
 if __name__=='__main__':
 	print 'Parent process %s.' % os.getpid()
-	p = Pool(1)  # at the same time,running number # 同时运行的数目
-	taskListnum = 1  # alignment number # 列队中的数目
+	p = Pool(2)  # at the same time,running number # 同时运行的数目
+	taskListnum = 2  # alignment number # 列队中的数目
 	for i in range(taskListnum):
 		if taskListnum < 10:
 			p.apply_async(myPool, args=("self"+str(i),))
