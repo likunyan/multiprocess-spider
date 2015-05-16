3de446fe7406b9a7e3fea68241a492e1ef4a0470
小李世界 <327125038@qq.com>
2015-05-16 13:17:01 +0800

    Update m-spider.py

--- m-spider.py
+++ m-spider.py
@@ -19,7 +19,6 @@ def open_text_file(source_text_file):
     
     
     def spider(text_line):
-        
         text_line = text_line.replace("\n", "")  # 替换上一步中，轮询到的每行结果中的换行字符为空白
         req_url = "http://"+text_line  # 因为source_text_file的域名是不带http://的，这边加下
         
@@ -41,7 +40,6 @@ def open_text_file(source_text_file):
                 
                 if utf8[0] in coding.lower() or utf8[1] in coding.lower():
                     source_is_utf8 = html_source
-                    
                     if "百家乐" in source_is_utf8:
                         with open(result_text_file, 'a') as output:
                             output.write("违规信息-百家乐"+" "+req_url+"\n")
@@ -61,7 +59,6 @@ def open_text_file(source_text_file):
                     else:  # 特殊标题的标记
                         # <title xmlns=...><title> 个人用
                         title = re.search(r'<title xmlns="">(.*)</title>', source_is_utf8, flags=re.I)
-                        
                         if title:
                             with open(result_text_file, 'a') as output:
                                 output.write(title.group(1)+" "+req_url+"\n")
@@ -73,7 +70,6 @@ def open_text_file(source_text_file):
                     source_no_utf8 = string.replace(source_no_utf8, '\r\n', '');
                     source_no_utf8 = string.replace(source_no_utf8, '\n', '');
                     title = re.search(r'<title>(.*?)</title>', source_no_utf8, flags=re.I)
-                    
                     if "百家乐" in source_no_utf8:
                         with open(result_text_file, 'a') as output:
                             output.write("违规信息-百家乐"+" "+req_url+"\n")
@@ -86,7 +82,6 @@ def open_text_file(source_text_file):
                             output.write(title.group(1)+" "+req_url+"\n")
                     else:
                         title = re.search(r'<title xmlns="">(.*)</title>', source_no_utf8, flags = re.I)
-                        
                         if title:
                             with open(result_text_file, 'a') as output:
                                 output.write(title.group(1)+" "+req_url+"\n")
@@ -117,18 +112,15 @@ if __name__=='__main__':
     p = Pool(number_of_at_the_same_time_the_process)  # at the same time,running number # 同时运行的数目
     
     for i in xrange(number_of_tasks):
-        
         if number_of_tasks < 10:
             p.apply_async(open_text_file, args=("self"+str(i),))
         elif number_of_tasks < 100:
-            
             if i < 10:
                 p.apply_async(open_text_file, args=("self0"+str(i),))
             else:  # that is 10 < i < 100
                 p.apply_async(open_text_file, args=("self"+str(i),))
                 
         elif number_of_tasks < 1000:
-            
             if i < 10:
                 p.apply_async(open_text_file, args=("self00"+str(i),))
             elif i < 100:
@@ -137,7 +129,6 @@ if __name__=='__main__':
                 p.apply_async(open_text_file, args=("self"+str(i),))
                 
         elif number_of_tasks < 10000:
-            
             if i < 10:
                 p.apply_async(open_text_file, args=("self000"+str(i),))
             elif i < 100:
