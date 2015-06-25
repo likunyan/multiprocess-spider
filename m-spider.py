5fad12871a85195e48d9299f1c000f5ca9b68b1b
小李世界 <327125038@qq.com>
2015-06-25 15:30:32 +0800

    Update m-spider.py

--- m-spider.py
+++ m-spider.py
@@ -1,9 +1,9 @@
-#coding=utf-8
 #!/usr/bin/python
+#coding=utf-8
 from urllib2 import Request, urlopen, URLError, HTTPError
 from multiprocessing import Pool
 import os
-import chardet
+import chardet # require to install
 import sys
 import string
 import urllib2
@@ -11,22 +11,46 @@ import re
 import time
 import sys
 # import socket
-# urllib2.socket.setdefaulttimeout(60) # Python2.6以前的版本,备用
+# urllib2.socket.setdefaulttimeout(30) # backup.before the Python 2.6
 
 def usage():
     print u'''
-    help: python m-spider.py <Process_number Text_line>
-
-    [Process_number]: running number of at the same time
-    [Text_line]: How much lines in the self file for each process
+    Usage：
+        >python m-spider.py <进程数 每个进程处理多少行网址>
+        
+    Example:
+        >m-spider.py 4 4
+    
+    Necessary:
+        1.document name:url.txt
+            1.网址一行两列,以空格间隔
+                *.每行第一列为虚拟主机提供商提供的测试网址，每行第二列为客户域名网址
+            2.不带http://
+        2.chardet
+    
+    Description:
+        运行结果会保存为*-result（*为模式匹配）
+     
+    Efficiency:
+        *.1 CPU Core : 35 process
+        *.2 CPU Core : 60 process
+        
+    Project:
+        https://github.com/likunyan/multiprocess-spider
+            
+    Follow:
+        李坤严 2015/06/25
+        https://www.likunyan.com
     '''
     
+#定义,文件名    
+text_file = "url.txt"    
+
 if len(sys.argv) is 3:
-    # running number of at the same time # 同时运行的数目   
-    number_of_at_the_same_time_the_process = int(sys.argv[1])
-    # 一个进程处理几行数据                                
+    #定义,进程数
+    number_of_processes = int(sys.argv[1])
+    #定义,每个进程处理多少行网址
     text_lines = int(sys.argv[2])
-    text_file = "self"
 elif len(sys.argv) is not 3:
     usage()
     sys.exit(1)
@@ -41,10 +65,11 @@ while True:
     if not buffer:  
         break  
     total_text_lines += buffer.count('\n')
+    print "url.txt总共" + str(total_text_lines) + "行网址"
 thefile.close()
 
 '''
-计算 『self』这个文件在指定『一个进程处理多少条数据』的时候，需要安排几个任务
+计算url.txt在指定『每个进程处理多少行网址』的时候，需要安排几个任务
 '''
 if total_text_lines%text_lines == 0:
     # alignment number # 列队中的数目
@@ -53,7 +78,7 @@ if total_text_lines%text_lines == 0:
 else:
     # 有余数的时候，需要多安排一个任务
     number_of_tasks = (total_text_lines/text_lines)+1    
-print "总共" + str(number_of_tasks) + "个任务"
+print "计划安排" + str(number_of_tasks) + "个任务"
 
 
 def open_text_file(i,start_line, end_line):
@@ -67,7 +92,7 @@ def open_text_file(i,start_line, end_line):
         except Exception, e:
             with open(result_text_file, 'a') as output:
                 output.write("url_Error "+str(e)+" "+req_url+"\n")
-            return 0 # 表示失败，需要重新访问每行第二列的备用网址
+            return 0 # 表示失败当前（即第一列）网址访问报错，需要重新访问第二列网址
         else:
             try:
                 html_source = urllib2.urlopen(req_url, timeout=60).read()
@@ -92,13 +117,13 @@ def open_text_file(i,start_line, end_line):
                     source_is_utf8 = string.replace(source_is_utf8, '\r\n', '');
                     source_is_utf8 = string.replace(source_is_utf8, '\n', '');
                     title = re.search(r'<title>(.*?)</title>', source_is_utf8, flags=re.I)
-                    #if m: # 如果标题不为空 则真，否则为假
+                    #if m: # 网址标题是否为空
                         #print title.group()
                     if title:
                         with open(result_text_file, 'a') as output:
                             output.write(title.group(1)+" "+req_url+"\n")
-                    else:  # 特殊标题的标记
-                        # <title xmlns=...><title> 个人用
+                    else:
+                        # <title xmlns=...><title> 特殊情况
                         title = re.search(r'<title xmlns="">(.*)</title>', source_is_utf8, flags=re.I)
                         
                         if title:
@@ -120,7 +145,7 @@ def open_text_file(i,start_line, end_line):
                         with open(result_text_file, 'a') as output:
                             output.write("违规信息-太阳城"+" "+req_url+"\n")
                             
-                    if title:  # 如果标题不为空 则真，否则为假
+                    if title:  # 网址标题是否为空
                         with open(result_text_file, 'a') as output:
                             output.write(title.group(1)+" "+req_url+"\n")
                     else:
@@ -138,12 +163,12 @@ def open_text_file(i,start_line, end_line):
         output.write("开始时间:"+time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())+"\n")
     print "进程"+str(i)+"开始"
     
-    result_text_file = str(i)+"-result"  # 执行结果以源文件名+result形式保存
+    result_text_file = str(i)+"-result"  # 执行结果以进程标志+result形式保存
     
     for text_line in open(text_file).readlines()[start_line:end_line]:  # 轮询源文件中的网址
         host_value = text_line.split() # 用空格分割字符串，并保存到列表
         status = spider(host_value[0])
-        if status == 0: # 如果start_line这个文本中第一列的网址能够访问的话，执行第二列中的网址
+        if status == 0: # 如果url.txt这个文本中第一列的网址不能够访问的话，执行第二列中的网址
             spider(host_value[1])
             
     with open("log", 'a') as output:
@@ -153,7 +178,7 @@ def open_text_file(i,start_line, end_line):
     
 if __name__=='__main__':
     print 'Parent process %s.' % os.getpid()
-    p = Pool(number_of_at_the_same_time_the_process)
+    p = Pool(number_of_processes)
     for i in xrange(number_of_tasks):
         i+=1 #从1开始
         p.apply_async(open_text_file, args=(i,text_lines*(i-1),text_lines*i,))
@@ -161,5 +186,4 @@ if __name__=='__main__':
     print 'Waiting for all subprocesses done...'
     p.close()
     p.join()
-    os.system("amh mysql start")        #怕mysql挂掉
     print 'All subprocesses done.'
